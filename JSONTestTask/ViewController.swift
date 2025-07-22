37b5d979eca72b7417e845ec5ac5dfcea0265388
Daniil <64897743+IvankovDaniil@users.noreply.github.com>
2025-07-22 09:02:51 +0700

    Initial Commit

---
+++ JSONTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONTestTask
+//
+//  Created by Даниил Иваньков on 22.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
